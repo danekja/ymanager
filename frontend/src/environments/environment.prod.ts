7b8b12229d60f742a99fea98f1caae1400de3cef
Václav Jirák <jirakv95@gmail.com>
2019-04-17 00:48:05 +0200

    Re #7264 HTTP Post test fixed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.99.100:9080/' // TODO Change to production url
+  apiUrl: 'http://localhost:9080/' // TODO Change to production url
 };
