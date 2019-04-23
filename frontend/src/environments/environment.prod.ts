7df5cfd9c41abaa43bcdf6c2fcf6a7da4069a26f
Hung Hoang <hungig.manni@gmail.com>
2019-04-23 14:57:51 +0200

    Revert "Re #7264 Angular project initialized"
    This reverts commit 603d90c558ac4a9926abdebc218e1df36b42e7b3.

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://192.168.99.100:9080/' // TODO Change to production url
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://192.168.99.100:9080/'
-};
