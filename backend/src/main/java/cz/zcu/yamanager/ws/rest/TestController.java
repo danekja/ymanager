07301c3b8b1691414978a4d8d9027560dd99a33f
Hung Hoang <hungig.manni@gmail.com>
2019-04-23 14:57:51 +0200

    Revert "Re #7264 CORS fix for development enviroment"
    This reverts commit abd277d86ae416562f9ba9ef98cf54afb46e059a.

--- backend/src/main/java/cz/zcu/yamanager/ws/rest/TestController.java
+++ backend/src/main/java/cz/zcu/yamanager/ws/rest/TestController.java
@@ -1,13 +1,11 @@
 package cz.zcu.yamanager.ws.rest;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController("/test")
 public class TestController {
 
