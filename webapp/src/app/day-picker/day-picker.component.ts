2c8bd4eb49260e988743d6500d4ac69662139885
Václav Jirák <jirakv95@gmail.com>
2019-05-25 15:46:30 +0200

    Re #7500 Day picker component modified - displaying vacations + emits change of month

--- webapp/src/app/day-picker/day-picker.component.ts
+++ webapp/src/app/day-picker/day-picker.component.ts
@@ -1,6 +1,7 @@
-import { Component, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
-import { CalendarView } from 'angular-calendar';
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { CalendarView, CalendarEvent } from 'angular-calendar';
 import {LocalizationService} from '../localization/localization.service';
+import {VacationType} from '../enums/common.enum';
 
 @Component({
   selector: 'app-day-picker',
@@ -12,14 +13,24 @@ export class DayPickerComponent {
 
   private locale;
 
+  private vacationType = VacationType;
+
+  private currentMonth: number;
+
   // Type of calendar (constant)
   view: CalendarView = CalendarView.Month;
 
   // Selected date for this component's purpose
-  private viewDate: Date = new Date();
+  private viewDate: Date;
+
+  // Events which are shown in calendar (title = VacationType)
+  @Input() events: CalendarEvent[] = [];
 
   // EventEmitter informing about changes of selected date
   @Output() selectedDate = new EventEmitter<Date>();
+
+  // EventEmitter informing about changes of selected month
+  @Output() selectedMonth = new EventEmitter<number>();
 
   constructor(private localizationService: LocalizationService) {
     this.locale = localizationService.defaultLanguage;
@@ -27,6 +38,10 @@ export class DayPickerComponent {
       .subscribe((data) => {
         this.locale = data;
       });
+
+    this.viewDate = new Date();
+    this.currentMonth = this.viewDate.getMonth();
+    console.log(this.currentMonth);
   }
 
   /**
@@ -38,5 +53,13 @@ export class DayPickerComponent {
   private dayClicked({ date }: { date: Date }): void {
     this.viewDate = date;
     this.selectedDate.emit(date);
+  }
+
+  private setMonth(newMonth: number): void {
+    this.currentMonth = newMonth % 12;
+    if (this.currentMonth < 0) {
+      this.currentMonth = 11;
+    }
+    this.selectedMonth.emit(this.currentMonth);
   }
 }
--- webapp/src/app/day-picker/day-picker.module.ts
+++ webapp/src/app/day-picker/day-picker.module.ts
@@ -3,6 +3,7 @@ import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { DayPickerComponent } from './day-picker.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
 
 @NgModule({
   declarations: [ DayPickerComponent ],
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
                     provide: DateAdapter,
                     useFactory: adapterFactory
                   }),
-                  BrowserAnimationsModule
+                  BrowserAnimationsModule,
+                  MatButtonModule
                 ],
 })
 export class DayPickerModule {}
