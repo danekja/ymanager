2f728b98f39f0f8619f16d83dd7bc16c8f881587
Václav Jirák <jirakv95@gmail.com>
2019-06-05 12:26:11 +0200

    Remaining sickdays is now displayed correctly

--- webapp/src/app/models/user.model.ts
+++ webapp/src/app/models/user.model.ts
@@ -24,6 +24,7 @@ export interface UserProfile {
   photo: string;
   vacationCount: number;
   sickDayCount: number;
+  takenSickDayCount: number;
   status: RequestStatus;
   role: UserType;
   notification: Date;
