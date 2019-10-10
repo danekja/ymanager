5ed6fa078296b81bb250cbf5e2db25702c37bf26
Jakub Danek <jakub.danek@yoso.fi>
2019-10-10 11:27:06 +0200

    re #37 dummy spring security configuration

---
+++ server/src/main/java/org/danekja/ymanager/SecurityConfiguration.java
@@ -0,0 +1,57 @@
+package org.danekja.ymanager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    /**
+     * This is a dummy authentication provider which does not check password at all.
+     * <p>
+     * To be replaced with OAuth2 client
+     *
+     * @param userDetailsService
+     * @return
+     */
+    @Bean
+    public AuthenticationProvider dummyAuthManager(UserDetailsService userDetailsService) {
+        return new AbstractUserDetailsAuthenticationProvider() {
+
+            private final Logger LOG = LoggerFactory.getLogger("DummyLogger");
+
+            @Override
+            protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+
+            }
+
+            @Override
+            protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+                return userDetailsService.loadUserByUsername(username);
+            }
+        };
+    }
+
+}
