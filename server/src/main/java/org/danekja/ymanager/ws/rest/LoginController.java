7c8f251fcf7ed808903425ce2f1d5c63bdab8ece
Jakub Danek <jakub.danek@yoso.fi>
2019-10-16 22:23:55 +0200

    re #29 hackity login endpoint, check todo, needs reimplementation

---
+++ server/src/main/java/org/danekja/ymanager/ws/rest/LoginController.java
@@ -0,0 +1,54 @@
+package org.danekja.ymanager.ws.rest;
+
+import org.danekja.ymanager.business.AuthorizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private AuthorizationService authorizationService;
+
+    private RequestCache requestCache = new HttpSessionRequestCache();
+
+    /**
+     * Hackity way of providing connecting apps with means to say where to redirect after login.
+     * Example call: GET /api/login/google?target=http:/myfrontapp.com/
+     * <p>
+     * TODO most likely should be dropped after OAuth2 authentication is triggered and handled by front-end,
+     * TODO while backend only accepts the necessary token.
+     *
+     * @param target
+     * @param provider
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+    @GetMapping("login/{provider}")
+    public void login(@RequestParam("target") String target,
+                      @PathVariable("provider") String provider,
+                      HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (authorizationService.isSignedIn()) {
+            response.sendRedirect(target);
+        } else {
+            switch (provider) {
+                case "google":
+                    this.requestCache.saveRequest(request, response);
+                    response.sendRedirect("/api/oauth2/authorization/google");
+                    break;
+                default:
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            }
+
+        }
+    }
+}
